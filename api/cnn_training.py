33dff2b08190f6f1757dd9020b7d12fe579c21f8
Vlctour <107158737+Vlctour@users.noreply.github.com>
2024-01-20 17:15:21 -0800

    testing more models, pushing predict.py

--- api/cnn_training.py
+++ api/cnn_training.py
@@ -3,10 +3,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, GlobalAveragePooling2D
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 from tensorflow.keras.regularizers import l2
 import tensorflow.keras.utils as utils
+
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import EfficientNetB0
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,6 +19,8 @@ import cv2
 import time
 LOG_FILE_NAME = "Garbage-Classification-Logs-{}".format(int(time.time()))
 tensorboard = TensorBoard(log_dir='logs/{}'.format(LOG_FILE_NAME))
+
+IMG_SIZE = 50
 
 CATEGORIES = [
     "battery",
@@ -46,23 +52,26 @@ y_train = utils.to_categorical(y_train, num_classes=len(CATEGORIES))
 y_test = utils.to_categorical(y_test, num_classes=len(CATEGORIES))
 
 # building the model
+# Create MobileNetV2 base model
+mobile_model_base = MobileNetV2(weights='imagenet', include_top=False, input_shape=(IMG_SIZE, IMG_SIZE, 3))
+# Resnet base model
+# resnet_model_base = ResNet50(weights='imagenet', include_top = False, input_shape=(IMG_SIZE, IMG_SIZE, 3))
+# Eff base model
+# eff_model_base = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(IMG_SIZE, IMG_SIZE, 3))
+
+# # Freeze the base model layers
+for layer in mobile_model_base.layers:
+    layer.trainable = False
+
 # some notes: conv can be followed by either: maxpool or activation
 # when calling dense, if working with 2d, we must first call flatten since dense requires it
 # 3 known activations, relu, softmax, sigmoid
 model = Sequential()
-
-model.add(Conv2D(64, (3, 3), input_shape = X_train.shape[1:], kernel_regularizer=l2(0.01)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Conv2D(64, (3, 3), kernel_regularizer=l2(0.01)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Flatten())
+model.add(mobile_model_base)
+model.add(GlobalAveragePooling2D())
+# model.add(Flatten())
 model.add(Dense(64, kernel_regularizer=l2(0.01)))
 model.add(Dense(len(CATEGORIES), activation="softmax"))
-# model.add(Activation("relu"))
 
 # loss has: binary_crossentropy and categorical_crossentropy
 model.compile(optimizer="adam",
@@ -73,7 +82,7 @@ model.compile(optimizer="adam",
 FILE = "model_3.h5"
 checkpoint = ModelCheckpoint(FILE, monitor='val_accuracy', save_best_only=True)
 
-model.fit(X_train, y_train, epochs=10, validation_split=0.1, callbacks=[tensorboard, checkpoint])
+model.fit(X_train, y_train, epochs=5, validation_split=0.1, callbacks=[tensorboard, checkpoint])
 # extra stuff i needa look into batch_size=32, validation_split=0.1, validation_data=(X_test, y_test)
 
 # check validation loss and acc
@@ -89,13 +98,13 @@ new_model = tf.keras.models.load_model(FILE)
 predictions = new_model.predict([X_test])
 
 for i in range(0, 10):
-    # normalize to [0, 255]
-    img_normalized = cv2.normalize(X_test[i], None, 0, 255, cv2.NORM_MINMAX)
+    # # normalize to [0, 255]
+    # img_normalized = cv2.normalize(X_test[i], None, 0, 255, cv2.NORM_MINMAX)
 
-    # convert back to normal rgb
-    img_rgb = cv2.cvtColor(img_normalized.astype(np.uint8), cv2.COLOR_BGR2RGB)
+    # # convert back to normal rgb
+    # img_rgb = cv2.cvtColor(img_normalized.astype(np.uint8), cv2.COLOR_BGR2RGB)
 
     # predictions
     print(CATEGORIES[np.argmax(predictions[i])])
-    plt.imshow(img_rgb)
+    plt.imshow(X_test[i])
     plt.show()
